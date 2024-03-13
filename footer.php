e52eaa489fc42e0a4d2f70e82ad1c62c4c2f6cd7
Adam Isacson <adam@adams-mbp.myfiosgateway.com>
2024-03-13 17:10:13 -0400

    Added a link to CBP's dataset

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <?php
 
-	echo "<p class='footer-github'>Improve this resource: fork the <a target='_blank' href='https://github.com/isacson/cbpdata'>source code on GitHub</a></p>"
+	echo "<br><p class='footer-github'>This resource uses data pulled from U.S. Customs and Border Protection's <a target='_blank' href='https://www.cbp.gov/document/stats/nationwide-encounters'>&ldquo;Nationwide Encounters&rdquo; dataset</a>. Help improve it: fork the <a target='_blank' href='https://github.com/isacson/cbpdata'>source code on GitHub</a></p>"
 
 ?>
